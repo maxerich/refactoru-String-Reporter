0cb85bbfa35c60b5c8ab3404dabea4dc33619307
Max <maxerich@umich.edu>
2014-01-16 16:49:13 -0700

    String Reporter Folder

---
+++ main.js
@@ -0,0 +1,10 @@
+var name = prompt("Enter a word.");
+
+var res = (name);
+alert("You entered " + res + "\n" + "There are " + res.length 
+	+ " characters in the word " + "\n" + "The third character is " + res.charAt(2) 
+	+ "\n" + "Lowercase: " + res.toLowerCase() + "\n" + "Uppercase: " + res.toUpperCase() + "\n" + 
+	"Example: This " + res + " is the word you typed in." + "\n" + "Subword: " + res.substring(1,4) );
+
+
+
